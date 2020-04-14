1e982651473a1dd50653426638dfeb6fece95887
Come de Germay <cnd3617@ic.ac.uk>
2020-04-14 21:14:14 +0200

    Refactor of cpu_fetcher.py by adding a function (_get_brand_and_product_type and modifying the websites.

--- cpu_fetcher.py
+++ cpu_fetcher.py
@@ -29,11 +29,11 @@ class CpuFetcher(AbstractFetcher):
                 "AMD Ryzen": "https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wpr_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_3-63|557-8865,8660,8617,10578.html",
             },
             GrosBill: {
-                "Intel Core": "https://www.grosbill.com/3-processeur_intel-processeur_intel_core_i9-type-informatique#filtre_mini=-1&filtre_maxi=-1&filtre_marque=INTEL",
+                "Intel Core": "https://www.grosbill.com/2-processeur_intel-cat-informatique?page=1&tri=w&filtre_page=100&mode=listing&filtre_type_produit=processeur",
                 "AMD Ryzen": "https://www.grosbill.com/2-processeur_amd-cat-informatique?page=1&tri=w&filtre_page=50&mode=listing&filtre_type_produit=processeur#filtre_mini=-1&filtre_maxi=-1&filtre_marque=AMD"
             },
             Cybertek: {
-                "Intel Core": "https://www.cybertek.fr/processeur-5/amd-23.aspx?crits=3789%3a3777%3a3694%3a3987",
+                "Intel Core": "https://www.cybertek.fr/processeur-5/intel-6.aspx?crits=3778",
                 "AMD Ryzen": "https://www.cybertek.fr/processeur-5/amd-23.aspx?crits=3789%3a3777%3a3694%3a3987"
             },
             LDLC: {
@@ -46,13 +46,7 @@ class CpuFetcher(AbstractFetcher):
             },
         }
 
-    def _extract_intel_product_data(self, product_description) -> Tuple[Optional[str], Optional[str]]:
-        brand = "INTEL"
-        brands = {"INTEL": ["Core i9"]}
-        families = {
-            "CORE I9":    ["9900KF"]
-        }
-
+    def _get_brand_and_product_type(self, product_description, brand: str, brands: dict, families: dict) -> Tuple[Optional[str], Optional[str]]:
         family = self.find_exactly_one_element(brands[brand], product_description)
         if not family:
             logger.warning(f"Brand not found in product [{product_description}]")
@@ -66,6 +60,14 @@ class CpuFetcher(AbstractFetcher):
         product_type = f"{family} {sub_product_type}"
 
         return brand, product_type
+
+    def _extract_intel_product_data(self, product_description) -> Tuple[Optional[str], Optional[str]]:
+        brand = "INTEL"
+        brands = {"INTEL": ["Core i9"]}
+        families = {
+            "CORE I9":    ["9900KF"]
+        }
+        return self._get_brand_and_product_type(product_description, brand, brands, families)
 
     def _extract_amd_product_data(self, product_description) -> Tuple[Optional[str], Optional[str]]:
         brand = "AMD"
@@ -76,19 +78,7 @@ class CpuFetcher(AbstractFetcher):
             "RYZEN 7": ["2700X", "3700X", "3800X"],
             "RYZEN 9": ["3900X", "3950X"],
         }
-        family = self.find_exactly_one_element(brands[brand], product_description)
-        if not family:
-            logger.warning(f"Brand not found in product [{product_description}]")
-            return None, None
-
-        sub_product_type = self.find_exactly_one_element(families[family], product_description)
-        if not sub_product_type:
-            logger.warning(f"Product type not found in product [{product_description}]")
-            return None, None
-
-        product_type = f"{family} {sub_product_type}"
-
-        return brand, product_type
+        return self._get_brand_and_product_type(product_description, brand, brands, families)
 
     def _extract_product_data(self, product_description) -> Tuple[Optional[str], Optional[str]]:
         extract_mapping = {
