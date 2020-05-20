d2ee8c2d6d7101f6bf6d677c0ec7908073ff2ba5
Richard Dally <r.dally@protonmail.com>
2020-05-20 23:19:17 +0200

    Radeon GPU are now handled !

--- gpu_fetcher_test.py
+++ gpu_fetcher_test.py
@@ -1,12 +1,10 @@
-# coding: utf-8
-
 import unittest
-from nvidia_fetcher import NVidiaFetcher
+from gpu_fetcher import GpuFetcher
 
 
 class TestFindExactlyOneElement(unittest.TestCase):
     def setUp(self) -> None:
-        self.fetcher = NVidiaFetcher(database=None)
+        self.fetcher = GpuFetcher(database=None)
 
     @unittest.skip("TODO")
     def test_case_1(self):
@@ -18,46 +16,58 @@ class TestFindExactlyOneElement(unittest.TestCase):
 
 class TestExtractProductData(unittest.TestCase):
     def setUp(self) -> None:
-        self.fetcher = NVidiaFetcher(database=None)
+        self.fetcher = GpuFetcher(database=None)
 
-    def test_case_1(self):
-        product_description = 'Palit GeForce GTX 1660 Ti StormX, 6 Go'
+    def test_nvidia_case_1(self):
+        product_description = 'Palit GeForce RTX 2080 Ti StormX, 6 Go'
         brand, product_type = self.fetcher._extract_product_data(product_description)
         self.assertEqual('PALIT', brand)
-        self.assertEqual('1660 TI', product_type)
+        self.assertEqual('2080 TI', product_type)
 
-    def test_case_2(self):
-        product_description = 'Zotac Gaming GeForce GTX 1060 AMP Edition, 6 Go + jeu offert ! '
+    def test_nvidia_case_2(self):
+        product_description = 'Zotac Gaming GeForce RTX 2060 AMP Edition, 6 Go + jeu offert ! '
         brand, product_type = self.fetcher._extract_product_data(product_description)
         self.assertEqual('ZOTAC', brand)
-        self.assertEqual('1060', product_type)
+        self.assertEqual('2060', product_type)
 
-    def test_case_3(self):
+    def test_nvidia_case_3(self):
         product_description = 'MSI 2060 SuPeR'
         brand, product_type = self.fetcher._extract_product_data(product_description)
         self.assertEqual('MSI', brand)
         self.assertEqual('2060 SUPER', product_type)
 
-    def test_case_4(self):
-        product_description = 'GIGABYTE GeForce® GTX  1050TI D5 4G'
+    def test_nvidia_case_4(self):
+        product_description = 'GIGABYTE GeForce® RTX 2070 D5 4G'
         brand, product_type = self.fetcher._extract_product_data(product_description)
         self.assertEqual('GIGABYTE', brand)
-        self.assertEqual('1050 TI', product_type)
+        self.assertEqual('2070', product_type)
 
-    def test_case_5(self):
+    def test_nvidia_case_5(self):
         product_description = 'MSI RTX 3080 Ti 6 Go'
         brand, product_type = self.fetcher._extract_product_data(product_description)
         self.assertEqual('MSI', brand)
         self.assertEqual('3080 TI', product_type)
 
-    def test_case_6(self):
+    def test_nvidia_case_6(self):
         product_description = 'RTX 3070 GAINWARD'
         brand, product_type = self.fetcher._extract_product_data(product_description)
         self.assertEqual('GAINWARD', brand)
         self.assertEqual('3070', product_type)
 
-    def test_case_7(self):
+    def test_nvidia_case_7(self):
         product_description = 'RTX PNY 3080'
         brand, product_type = self.fetcher._extract_product_data(product_description)
         self.assertEqual('PNY', brand)
         self.assertEqual('3080', product_type)
+
+    def test_radeon_case_1(self):
+        product_description = "MSI Radeon RX 5700 XT EVOKE OC (v2) + 2 jeux offerts ! "
+        brand, product_type = self.fetcher._extract_product_data(product_description)
+        self.assertEqual('MSI', brand)
+        self.assertEqual('5700 XT', product_type)
+
+    def test_radeon_case_2(self):
+        product_description = "ASRock Radeon RX 570 Phantom Gaming D OC (8 Go)"
+        brand, product_type = self.fetcher._extract_product_data(product_description)
+        self.assertEqual("ASROCK", brand)
+        self.assertEqual("570", product_type)
