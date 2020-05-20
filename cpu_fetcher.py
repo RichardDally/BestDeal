76eca60c4a8c38d0b427c75cf29855d35dde9137
Richard Dally <r.dally@protonmail.com>
2020-05-20 23:48:09 +0200

    Update CPU urls

--- cpu_fetcher.py
+++ cpu_fetcher.py
@@ -20,24 +20,24 @@ class CpuFetcher(AbstractFetcher):
         Return product types you want to appear in tweets (one for each)
         To tweet about all product types: self.database.find_distinct_product_types()
         """
-        return ["RYZEN 5 3600X", "RYZEN 9 3950X", "CORE I9 9900KF"]
+        return ["RYZEN 5 3600X", "RYZEN 7 3700X", "RYZEN 9 3950X", "CORE I9 9900KF", "CORE I9 9900K"]
 
     def _get_source_product_urls(self) -> Dict[type(Source), Dict[str, str]]:
         return {
             TopAchat: {
-                "Intel Core": "https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wpr_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_3-61|557-8827.html",
-                "AMD Ryzen": "https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wpr_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_3-63|557-8865,8660,8617,10578.html",
+                "INTEL": "https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wpr_puis_marque_est_INTEL.html",
+                "AMD": "https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wpr_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_3-63|557-8865,8660,8617,10578.html",
             },
             GrosBill: {
-                "Intel Core": "https://www.grosbill.com/2-processeur_intel-cat-informatique?page=1&tri=w&filtre_page=100&mode=listing&filtre_type_produit=processeur",
-                "AMD Ryzen": "https://www.grosbill.com/2-processeur_amd-cat-informatique?page=1&tri=w&filtre_page=50&mode=listing&filtre_type_produit=processeur#filtre_mini=-1&filtre_maxi=-1&filtre_marque=AMD"
+                "INTEL": "https://www.grosbill.com/2-processeur_intel-cat-informatique?page=1&tri=w&filtre_page=100&mode=listing&filtre_type_produit=processeur",
+                "AMD": "https://www.grosbill.com/2-processeur_amd-cat-informatique?page=1&tri=w&filtre_page=50&mode=listing&filtre_type_produit=processeur#filtre_mini=-1&filtre_maxi=-1&filtre_marque=AMD"
             },
             Cybertek: {
-                "Intel Core": "https://www.cybertek.fr/processeur-5/intel-6.aspx?crits=3778",
+                "Intel Core": "https://www.cybertek.fr/processeur-5/intel-6.aspx",
                 "AMD Ryzen": "https://www.cybertek.fr/processeur-5/amd-23.aspx?crits=3789%3a3777%3a3694%3a3987"
             },
             LDLC: {
-                "Intel Core": "https://www.ldlc.com/informatique/pieces-informatique/processeur/c4300/+fb-C000000192+fv579-15953.html?sort=1",
+                "Intel Core": "https://www.ldlc.com/informatique/pieces-informatique/processeur/c4300/+fb-C000000192+fdi-1+foms-1+fv579-3857,5524,5912,6023,6114,7779,15953.html?sort=1",
                 "AMD Ryzen": "https://www.ldlc.com/informatique/pieces-informatique/processeur/c4300/+fv579-15490,15637,16016,17684.html?sort=1"
             },
             MindFactory: {
@@ -63,9 +63,10 @@ class CpuFetcher(AbstractFetcher):
 
     def _extract_intel_product_data(self, product_description) -> Tuple[Optional[str], Optional[str]]:
         brand = "INTEL"
-        brands = {"INTEL": ["Core i9"]}
+        brands = {"INTEL": ["Core i7", "Core i9"]}
         families = {
-            "CORE I9":    ["9900KF"]
+            "CORE I7": ["9700K", "10700K"],
+            "CORE I9": ["9900KF", "9900K"]  # Order does matter.
         }
         return self._get_brand_and_product_type(product_description, brand, brands, families)
 
