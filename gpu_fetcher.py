e3412587a87737ac5044abe3650635b4f3ea4104
Richard Dally <r.dally@protonmail.com>
2021-10-24 09:32:29 +0200

    Update urls

--- gpu_fetcher.py
+++ gpu_fetcher.py
@@ -44,50 +44,24 @@ class GpuFetcher(AbstractFetcher):
                 "RADEON": "https://www.pcw.fr/232-amd-radeon",
                 "RTX": "https://www.pcw.fr/210-nvidia-geforce-rtx",
             },
-            TopAchat:
-            {
-                # "2060": "https://bit.ly/2CMzlkr",
-                #"2070": "https://bit.ly/2FMNlg0",
-                #"2080": "https://bit.ly/2JQck65",
-                # "2060 Super": "https://bit.ly/2Y1r2NL",
-                # "2070 Super": "https://bit.ly/2YRb8Tj",
-                #"2080 Super": "https://bit.ly/2Yp0AJT",
-                #"2080 TI": "https://bit.ly/2TG5EXT",
-                "RTX 3 Stock": "https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wgfx_pcie_puis_f_est_58-11447,11884,11445,11883,11446|s-1.html",
+            TopAchat: {
+                "RTX 3 Stock": "https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wgfx_pcie_puis_f_est_58-11733,11575,11447,11884,11445,11883,11446|s-1.html",
                 "RADEON": "https://www.topachat.com/pages/produits_cat_est_micro_puis_rubrique_est_wgfx_pcie_puis_f_est_58-8743,10812,10851,8741,10586,10587,8742.html"
             },
-            GrosBill:
-            {
-                "Nvidia": "https://www.grosbill.com/2-carte_graphique-cat-informatique?tri=P&filtre_page=100&mode=listing&filtre_type_produit=carte_graphique&crits=4533%3a4145%3a4530%3a4146%3a3013",
+            GrosBill: {
+                "Nvidia": "https://www.grosbill.com/2-carte_graphique-cat-informatique?tri=P&filtre_page=100&mode=listing&filtre_type_produit=carte_graphique&crits=4289%3a4229%3a4144%3a4533%3a4145%3a4530%3a4146%3a3013",
                 "RADEON": "https://www.grosbill.com/3-carte_graphique-ati-type-informatique?page=1&tri=P&filtre_page=100&mode=listing&filtre_type_produit=carte_graphique"
             },
-            RueDuCommerce:
-            {
+            RueDuCommerce: {
                 # TODO: find workaround
                 # "2080 TI": "https://www.rueducommerce.fr/rayon/composants-16/carte-graphique-nvidia-1913?sort=prix-croissants&view=list&marchand=rue-du-commerce&it_card_chipset_serie=geforce-rtx-2080-ti"
             },
-            Cybertek:
-            {
-                # "2060": "https://bit.ly/2OITRYd",
-                # "2060 SUPER": "https://bit.ly/2OSyMe2",
-                # "2070": "https://bit.ly/2FElIo5",
-                # "2070 SUPER": "https://bit.ly/2rzczcX",
-                # "2080": "https://bit.ly/2TIQ4uM",
-                # "2080 SUPER": "https://bit.ly/2qOGE8y",
-                # "2080 Ti": "https://bit.ly/2JSMgaD",
-                "RTX 3 Stock": "https://www.cybertek.fr/carte-graphique-6.aspx?crits=4533%3a4145%3a4530%3a4146&order=p%3aa%3b",
+            Cybertek: {
+                "RTX 3 Stock": "https://www.cybertek.fr/carte-graphique-6.aspx?crits=4289%3a4229%3a4144%3a4533%3a4145%3a4530%3a4146",
                 "RADEON": "https://www.cybertek.fr/carte-graphique-6.aspx?crits=4053%3a4084%3a3707%3a3990%3a3991%3a3708&order=p%3aa%3b"
             },
-            LDLC:
-            {
-                "2060": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+foms-1+fv1026-5801+fv121-17312.html",
-                "2060 SUPER": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+foms-1+fv1026-5801+fv121-17729.html",
-                "2070": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+foms-1+fv1026-5801+fv121-16760.html",
-                "2070 SUPER": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+foms-1+fv1026-5801+fv121-17730.html",
-                "2080": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+foms-1+fv1026-5801+fv121-16758.html",
-                "2080 SUPER": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+foms-1+fv1026-5801+fv121-17731.html",
-                "2080 Ti": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+foms-1+fv1026-5801+fv121-16759.html",
-                "RTX 3 Stock": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+fdi-1+fv1026-5801+fv121-19183,19185,19800,19801.html",
+            LDLC: {
+                "RTX 3 Stock": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+fdi-1+fv1026-5801+fv121-19183,19184,19185,19365,19509,19800,19801.html",
                 "RADEON": "https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+fdi-1+fi131-l4h8+foms-1+fv1026-5800+fv121-15666,15667,17714,17715,18248,18293.html?sort=1"
             },
             Materiel:
