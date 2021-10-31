683e6b0b8277ba14e2aa16e3c93abef92f350463
Richard Dally <r.dally@protonmail.com>
2021-10-31 11:52:05 +0100

    Improve frontend with criteria select boxes (not fully working yet)

--- bestdeal/frontend/streamlit_app.py
+++ bestdeal/frontend/streamlit_app.py
@@ -1,3 +1,4 @@
+from typing import List
 import streamlit as st
 from pymongo import MongoClient
 from datetime import datetime, timezone
@@ -8,10 +9,33 @@ from bestdeal.core.pricedatabase import PriceDatabase
 mc = MongoClient(st.secrets["MONGODB_CONNECTION_STRING"])
 db = PriceDatabase(collection_name="GPU", client=mc)
 
-product_types = db.find_distinct_product_types()
-selected_product_type = st.sidebar.selectbox("Product type", product_types)
+# product_types = db.find_distinct_product_types()
 selected_date = st.sidebar.date_input(label="Date", value=datetime.now(timezone.utc))
+formatted_selected_date = selected_date.strftime("%Y%m%d")
 
+# Find available product
+available_product_types: List[str] = db.find_distinct_criteria_by_date("product_type", formatted_selected_date)
+if not available_product_types:
+    st.error(f"No available records for date [{formatted_selected_date}]")
+    st.stop()
 
-for post in db.find_all_posts_by_product_type(selected_product_type, selected_date.strftime("%Y%m%d")):
-    st.write(post)
+# Fill filters
+available_sources: List[str] = db.find_distinct_criteria_by_date("source", formatted_selected_date)
+available_brands: List[str] = db.find_distinct_criteria_by_date("product_brand", formatted_selected_date)
+
+# Filter selection
+selected_product_type = st.sidebar.selectbox("Product type", available_product_types)
+selected_source = st.sidebar.selectbox("Source", available_sources)
+selected_brand = st.sidebar.selectbox("Brand", available_brands)
+
+search = st.sidebar.button(label="Search")
+
+if search and selected_product_type:
+    mongo_cursor = db.find_all_posts_by_product_type(selected_product_type, selected_date.strftime("%Y%m%d"))
+    if mongo_cursor.count():
+        for post in mongo_cursor:
+            st.write(post)
+    else:
+        st.info(f"Nothing to display for [{selected_product_type}]")
+else:
+    st.info(f"Select a product type and click on Search")
